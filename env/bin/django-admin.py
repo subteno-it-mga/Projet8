da71e7411f84b133c845071ff89bab340d942883
Martin Gaucher <martin.gaucher@subteno-it.fr>
2019-09-17 11:44:55 +0200

    [ADD] Installation of Django / Initi DB

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/martingaucher/Documents/OpenClassrooms/Projet/Projet 8 - Subsitute/Projet8/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
