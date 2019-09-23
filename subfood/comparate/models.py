9e6651145845ee68a1188b0d5491ab747d3be550
Martin Gaucher <martin.gaucher@subteno-it.fr>
2019-09-23 10:19:37 +0200

    [ADD] Create first models and migrate

--- subfood/comparate/models.py
+++ subfood/comparate/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=200)
+
+class Products(models.Model):
+    nutriscore = models.CharField(max_length=1)
+    categories = models.ManyToManyField(Categories)
+    vegg = models.BooleanField()
+    vegan = models.BooleanField()
+    fat = models.FloatField(max_length=10)
+    sugar = models.FloatField(max_length=10)
+    salt = models.FloatField(max_length=10)
+    origin = models.CharField(max_length=200)
+
+class Favorite(models.Model):
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ForeignKey(Products, on_delete=models.CASCADE)
