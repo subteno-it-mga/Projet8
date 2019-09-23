4ee13ecd1e4e5a29405892bafe14922290e0eeea
Martin Gaucher <martin.gaucher@subteno-it.fr>
2019-09-23 10:30:39 +0200

    [IMP] Add name for products and manage admin space

--- subfood/comparate/models.py
+++ subfood/comparate/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User
 class Categories(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Products(models.Model):
+    name = models.CharField(max_length=100)
     nutriscore = models.CharField(max_length=1)
     categories = models.ManyToManyField(Categories)
     vegg = models.BooleanField()
@@ -14,6 +18,12 @@ class Products(models.Model):
     salt = models.FloatField(max_length=10)
     origin = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Favorite(models.Model):
     users = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.users
