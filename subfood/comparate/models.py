4c2952c7825f2aa4b0be8ffd1f58846a21f62398
Martin Gaucher <martin.gaucher@subteno-it.fr>
2019-09-24 11:55:01 +0200

    [WIP] Search the term, process it then display on a product page

--- subfood/comparate/models.py
+++ subfood/comparate/models.py
@@ -4,11 +4,15 @@ from django.contrib.auth.models import User
 class Categories(models.Model):
     name = models.CharField(max_length=200)
 
+<<<<<<< Updated upstream
     def __str__(self):
         return self.name
 
 class Products(models.Model):
     name = models.CharField(max_length=100)
+=======
+class Products(models.Model):
+>>>>>>> Stashed changes
     nutriscore = models.CharField(max_length=1)
     categories = models.ManyToManyField(Categories)
     vegg = models.BooleanField()
@@ -18,6 +22,7 @@ class Products(models.Model):
     salt = models.FloatField(max_length=10)
     origin = models.CharField(max_length=200)
 
+<<<<<<< Updated upstream
     def __str__(self):
         return self.name
 
@@ -27,3 +32,8 @@ class Favorite(models.Model):
 
     def __str__(self):
         return self.users
+=======
+class Favorite(models.Model):
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ForeignKey(Products, on_delete=models.CASCADE)
+>>>>>>> Stashed changes
